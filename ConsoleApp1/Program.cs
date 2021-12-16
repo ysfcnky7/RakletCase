77298b26c9e6fe7475c55f0d318cf76b02c4692e
Yusuf Çankaya <ysfcnky7@hotmail.com>
2021-12-16 15:57:26 +0300

    hello world

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char resultValue = NonRepeatFunctionReturnValue("aaaaaabbbsasdweqwe");
+            //char resultValue = NonRepeatFunctionReturnValue("aaaaaabbbsasweweq");
+            //char resultValue = NonRepeatFunctionReturnValue("qaaaaaabbbsaswewe");
+            //char resultValue = NonRepeatFunctionReturnValue("aaaaaabbbsaswewe");
+
+            if (resultValue != '\0')
+                Console.WriteLine("Sonuc : " + resultValue + " ");
+            else
+                Console.WriteLine("Tekrar etmeyen karakter bulunamadı");
+
+            Console.ReadKey();
+        }
+
+        private static char NonRepeatFunctionReturnValue(string value)
+        {
+            for (var i = 0; i < value.Length; i++)
+            {
+                char character = value[i];
+
+                if (value.IndexOf(character) == i && value.IndexOf(character, i + 1) == -1)
+                {
+                    return character;
+                }
+            }
+            return '\0';
+        }
+    }
+}
